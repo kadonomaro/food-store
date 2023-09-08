c1b08e259710b5ae856fc2306844f99e36b0a522
kadonomaro <denis.afer@gmail.com>
2023-09-08 23:28:48 +0700

    feat: update home-about components

--- components/home/HomeAbout.vue
+++ components/home/HomeAbout.vue
@@ -1,43 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    const list = [
+        {
+            title: "Широкий выбор меню",
+            text: "Мы&nbsp;понимаем, что у&nbsp;каждого человека свои вкусы и&nbsp;предпочтения, поэтому мы&nbsp;предлагаем многообразие блюд, начиная от&nbsp;популярных классических блюд до&nbsp;экзотических кухонь со&nbsp;всего мира. Вы&nbsp;всегда найдете что-то по&nbsp;своему вкусу в&nbsp;нашем меню.",
+            image: "menu.png",
+        },
+        {
+            title: "Высокое качество",
+            text: "Мы&nbsp;стремимся поставлять только самые свежие и&nbsp;качественные продукты. Наша команда шеф-поваров тщательно отбирает ингредиенты и&nbsp;готовит блюда только из&nbsp;самых лучших продуктов, чтобы обеспечить вам истинное гастрономическое наслаждение.",
+            image: "hat.png",
+        },
+        {
+            title: "Быстрая доставка",
+            text: "Время доставки для нас является приоритетом. Мы&nbsp;сократили время ожидания до&nbsp;минимума, чтобы вы&nbsp;могли наслаждаться своей любимой едой быстро и&nbsp;без лишних хлопот.",
+            image: "delivery.png",
+        },
+        {
+            title: "Удобство",
+            text: "Мы&nbsp;разработали удобный сайт, чтобы вы&nbsp;могли легко оформить заказ в&nbsp;любое удобное для вас время. Мы&nbsp;также предлагаем различные способы оплаты, чтобы сделать процесс заказа максимально гибким и&nbsp;удобным.",
+            image: "card.png",
+        },
+    ];
+</script>
 
 <template>
     <section class="home-about">
-        <h2 class="home-about__title page-title">Вкусная и полезная еда на дом</h2>
+        <h2 class="home-about__title page-title">Вкусная и&nbsp;полезная еда на&nbsp;дом</h2>
+        <div class="home-about__subtitle">
+            Food Store&nbsp;&mdash; это современная и&nbsp;инновационная компания доставки еды, которая предлагает своим
+            клиентам широкий ассортимент блюд самых разных кухонь. Мы&nbsp;стремимся не&nbsp;только удовлетворить голод,
+            но&nbsp;и&nbsp;доставить удовольствие от&nbsp;каждого заказа.
+        </div>
         <div class="home-about__inner">
-            <div class="home-about__text">
-                <p>
-                    Food Store&nbsp;&mdash; это современная и&nbsp;инновационная компания доставки еды, которая
-                    предлагает своим клиентам широкий ассортимент блюд самых разных кухонь. Мы&nbsp;стремимся
-                    не&nbsp;только удовлетворить голод, но&nbsp;и&nbsp;доставить удовольствие от&nbsp;каждого заказа.
-                </p>
-
-                <p>
-                    <b>Широкий выбор меню:</b> Мы&nbsp;понимаем, что у&nbsp;каждого человека свои вкусы
-                    и&nbsp;предпочтения, поэтому мы&nbsp;предлагаем многообразие блюд, начиная от&nbsp;популярных
-                    классических блюд до&nbsp;экзотических кухонь со&nbsp;всего мира. Вы&nbsp;всегда найдете что-то
-                    по&nbsp;своему вкусу в&nbsp;нашем меню.
-                </p>
-
-                <p>
-                    <b>Высокое качество:</b> Мы&nbsp;стремимся поставлять только самые свежие и&nbsp;качественные
-                    продукты. Наша команда шеф-поваров тщательно отбирает ингредиенты и&nbsp;готовит блюда только
-                    из&nbsp;самых лучших продуктов, чтобы обеспечить вам истинное гастрономическое наслаждение.
-                </p>
-
-                <p>
-                    <b>Быстрая доставка:</b> Время доставки для нас является приоритетом. Мы&nbsp;сократили время
-                    ожидания до&nbsp;минимума, чтобы вы&nbsp;могли наслаждаться своей любимой едой быстро и&nbsp;без
-                    лишних хлопот.
-                </p>
-
-                <p>
-                    <b>Удобство:</b> Мы&nbsp;разработали удобный сайт, чтобы вы&nbsp;могли легко оформить заказ
-                    в&nbsp;любое удобное для вас время. Мы&nbsp;также предлагаем различные способы оплаты, чтобы сделать
-                    процесс заказа максимально гибким и&nbsp;удобным.
-                </p>
-            </div>
-            <div class="home-about__image">
-                <img src="/assets/home/about/delivery.png" alt="" />
+            <div v-for="(card, index) in list" :key="index" class="home-about__item">
+                <home-about-card :card="card"></home-about-card>
             </div>
         </div>
     </section>
@@ -58,40 +54,31 @@
         }
     }
 
-    .home-about__inner {
-        @include media($bp-desktop-sm) {
-            display: flex;
-        }
-    }
-
-    .home-about__text {
+    .home-about__subtitle {
+        margin-bottom: 30px;
         font-size: 14px;
         line-height: 24px;
-        p {
-            &:not(:last-child) {
-                margin-bottom: 10px;
-            }
-        }
         @include media($bp-desktop-sm) {
-            margin-right: 40px;
+            margin-bottom: 60px;
             font-size: 16px;
             line-height: 28px;
-            p {
-                &:not(:last-child) {
-                    margin-bottom: 15px;
-                }
-            }
         }
     }
 
-    .home-about__image {
-        max-width: 80%;
-        margin: 0 auto;
+    .home-about__inner {
+        margin: 0 -5px;
         @include media($bp-desktop-sm) {
-            flex-shrink: 0;
-            flex-basis: 400px;
-            max-width: 400px;
-            margin: 0;
+            display: flex;
+            margin: 0 -10px;
+        }
+    }
+
+    .home-about__item {
+        padding: 0 5px 10px;
+        @include media($bp-desktop-sm) {
+            flex-basis: 25%;
+            max-width: 25%;
+            padding: 0 10px;
         }
     }
 </style>
---
+++ components/home/HomeAboutCard.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+    type Props = {
+        card: {
+            title: string;
+            text: string;
+            image: string;
+        };
+    };
+
+    defineProps<Props>();
+</script>
+
+<template>
+    <div class="home-about-card">
+        <div class="home-about-card__image">
+            <img :src="`/assets/home/about/${card.image}`" alt="" />
+        </div>
+        <div class="home-about-card__title">{{ card.title }}</div>
+        <div class="home-about-card__text" v-html="card.text"></div>
+    </div>
+</template>
+
+<style lang="scss">
+    .home-about-card {
+        height: 100%;
+        padding: 10px;
+        text-align: center;
+        border-radius: var(--border-radius--md);
+        background-color: #fff;
+        @include media($bp-desktop-sm) {
+            padding: 20px;
+        }
+    }
+
+    .home-about-card__image {
+        position: relative;
+        max-width: 150px;
+        margin: 0 auto;
+        &::before {
+            content: "";
+            display: block;
+            padding-bottom: 100%;
+        }
+        img {
+            @include center-absolute(0);
+        }
+        @include media($bp-desktop-sm) {
+            max-width: 200px;
+        }
+    }
+
+    .home-about-card__title {
+        margin-bottom: 5px;
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 20px;
+        @include media($bp-desktop-sm) {
+            margin-bottom: 10px;
+            font-size: 16px;
+            line-height: 24px;
+        }
+    }
+
+    .home-about-card__text {
+        font-size: 12px;
+        line-height: 20px;
+        @include media($bp-desktop-sm) {
+            font-size: 14px;
+            line-height: 22px;
+        }
+    }
+</style>
